d1d5036490b7039a0a030ad4ad269efb7498c967
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2015-04-20 19:06:12 -0700

    Updated cartridge support

--- start.js
+++ start.js
@@ -1,4 +1,8 @@
 //File exists only due to cartridge requirements
 //Cartridge always starts with start.js, doesn't read package.json
-var spawn = require('child_process').spawn;
-spawn('nohup', ['node', 'Server.js']);
+var spawn = require('child_process').spawn,
+	server = spawn('nohup', ['node', 'Server.js']);
+
+server.on("close", function(code) {
+	process.exit(code);
+})
